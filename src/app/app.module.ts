1ef88a306ff33131734f250226f9ab52ac70a669
Juuhcso <juuhtambasco@gmail.com>
2023-10-14 10:23:48 -0300

    projeto finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CardPromocionalComponent } from './components/card/card-promocional/card-promocional.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuComponent,
+    CardPromocionalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card/card-promocional/card-promocional.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-promocional',
+  templateUrl: './card-promocional.component.html',
+  styleUrls: ['./card-promocional.component.css']
+})
+export class CardPromocionalComponent {
+
+}
